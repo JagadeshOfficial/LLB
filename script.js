a913b67442b7e5208f1259d0453d5634268ef1f9
JAGADESWARARAO VANA <you@example.com>
2024-08-27 09:01:05 +0530

    LLB

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(function(dropdown) {
+        var btn = dropdown.querySelector('.dropbtn');
+        btn.addEventListener('click', function(event) {
+            event.preventDefault();
+            dropdowns.forEach(function(otherDropdown) {
+                if (otherDropdown !== dropdown) {
+                    otherDropdown.classList.remove('open');
+                }
+            });
+            dropdown.classList.toggle('open');
+        });
+    });
+
+    window.addEventListener('click', function(e) {
+        if (!e.target.matches('.dropbtn')) {
+            dropdowns.forEach(function(dropdown) {
+                dropdown.classList.remove('open');
+            });
+        }
+    });
+});
